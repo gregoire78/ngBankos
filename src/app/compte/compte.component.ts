2e4acbc15f3c63523ae8a77f0f0e25621116a0a5
Grégoire <greg.joncour@gmail.com>
2017-10-26 11:27:26 +0200

    emot 2

--- src/app/compte/compte.component.ts
+++ src/app/compte/compte.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Mouvement } from '../models/mouvement';
 
 @Component({
@@ -8,13 +8,6 @@ import { Mouvement } from '../models/mouvement';
 })
 export class CompteComponent implements OnInit {
   
-  tab: Array<any>=[
-    {title: 'Abonnement YP', somme: -300, desc: "desciption", date: new Date()},
-    {title: 'Abonnement YP', somme: +62, desc: "desciption", date: new Date()},
-    {title: 'salaire', somme: +50, desc: "desciption", date: new Date()},
-    {title: 'chose', somme: -69.99, desc: "desciption", date: new Date()}
-  ]
-
   compte:Array<Mouvement>=[
     new Mouvement('abonnement yo', -300, 'description', new Date()),
     new Mouvement('abonnement yo', -300, 'description', new Date()),
